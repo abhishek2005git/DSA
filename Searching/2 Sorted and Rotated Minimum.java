5e28235f672dfecb452aeba3c92fd037e354b1ab
Abhishek <abhishek.s.yadav@slrtce.in>
2024-12-20 15:06:07 +0530

    changes in 2

--- Searching/2 Sorted and Rotated Minimum.java
+++ Searching/2 Sorted and Rotated Minimum.java
@@ -8,19 +8,19 @@ Explanation: 1 is the minimum element in the array. */
 
 class Solution {
     public int findMin(int[] arr) {
-        int low = 0, high = arr.length - 1;
-        
-        while(low < high) {
-            if(arr[low] < arr[high])
-                return arr[low];
-                
-            int mid = (low + high) / 2;
-            
-            if(arr[mid] > arr[high])
-                low = mid + 1;
-            else
-                high = mid;
-        }
-        return arr[low];
+       int low = 0, high = arr.length - 1, ans = Integer.MAX_VALUE;
+       
+       while(low <= high) {
+           int mid = (low + high) / 2;
+           if(arr[low] <= arr[mid]) {
+               ans = Math.min(ans,arr[low]);
+               low = mid + 1;
+           }
+           else {
+               ans = Math.min(ans, arr[mid]);
+               high = mid - 1;
+           }
+       }
+       return ans;
     }
 }
