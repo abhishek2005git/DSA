1cc7958ff6f024b02f1548aab1eb24beb4064a23
Abhishek <abhishek.s.yadav@slrtce.in>
2024-12-20 14:27:20 +0530

    changes in peak element

--- Searching/4 Peak element.java
+++ Searching/4 Peak element.java
@@ -11,15 +11,21 @@ Explanation: arr[5] = 8 is a peak element because arr[4] < arr[5] > arr[6]. */
 class Solution {
 
     public int peakElement(int[] arr) {
-        int low = 0, high = arr.length - 1;
+        int n = arr.length;
+        if(n == 1) return 0;
+        if(arr[0] > arr[1]) return 0;
+        if(arr[n-1] > arr[n-2]) return n-1;
+        int low = 1, high = n - 2;
         
-        while(low < high) {
+        while(low <= high) {
             int mid = (low + high) / 2;
-            if(arr[mid] < arr[mid+1])
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1])
+                return mid;
+            else if(arr[mid] > arr[mid-1])
                 low = mid + 1;
             else
-                high = mid;
+                high = mid - 1;
         }
-        return high;
+        return -1;
     }
 }
